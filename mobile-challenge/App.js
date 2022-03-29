709d403d2fb8d65e29a173d437c26934fc3ea9d1
ericfrancis313 <48592818+ericfrancis313@users.noreply.github.com>
2022-03-28 22:00:25 -0400

    Added API functionalty and displays email

--- mobile-challenge/App.js
+++ mobile-challenge/App.js
@@ -1,13 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, ActivityIndicator } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  
+  constructor(props) {
+    super(props);
+    this.state={
+      isLoading: true,
+      peopleData: null,
+    }
+  }
+  // Add Api 
+  componentDidMount(){
+    return fetch("https://my.api.mockaroo.com/users.json?page=20&count=5&key=930279b0")
+      .then((response)=> response.json())
+      .then((responseJson)=>{
+        this.setState({
+          isLoading: false,
+          peopleData: responseJson.entries
+        })
+        
+      })
+      .catch((error)=>{
+        console.log(error)
+      });
+
+  }
+  render(){
+    //indicate that the page is loading
+    if(this.state.isLoading){
+      return(
+        <SafeAreaView style={styles.container}>
+          <ActivityIndicator/>
+        </SafeAreaView>
+      )
+    }else{
+      // map over the people data array 
+      let people = this.state.peopleData.map((val,key)=>{
+        return <SafeAreaView key={key} style={styles.item}>
+                  <Text>{val.email}</Text>
+               </SafeAreaView>
+      })
+      
+      return (
+        <SafeAreaView style={styles.container}>
+        {people}
+      </SafeAreaView>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +58,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  item:{
+    flex:1,
+    alignSelf: 'stretch',
+    alignItems:"center",
+  }
 });
